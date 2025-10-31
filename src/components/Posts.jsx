cd92641be6fc031a8e2263840586760dbc164ed2
sajib hossen <sajibhossen560@gmail.com>
2025-10-31 22:59:15 +0600

    delete a post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,32 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchPosts } from '../features/posts/Posts'
+import { deletePost, fetchPosts } from '../features/posts/Posts'
 
 const Posts = () => {
-  const dispatch = useDispatch()
-  const { isLoading, posts, error } = useSelector((state) => state.posts)
-  useEffect(() => {
-    dispatch(fetchPosts())
-  }, [dispatch])
+    const dispatch = useDispatch()
+    const {isLoading , posts ,error} = useSelector((state => state.posts))
+    useEffect(() =>{
+        dispatch(fetchPosts())
+    },[dispatch])
+
+    const handleDelete = (id) =>{
+      dispatch(deletePost(id))
+    }
   return (
-    <div>
-      {isLoading && <h3>Loading...</h3>}
-      {error && <h4>{error}</h4>}
-      {posts &&
-        posts.map((post) => {
-          return (
-            <article className="flex flex-col bg-white rounded-2xl shadow-md p-5 hover:shadow-lg transition-shadow duration-300">
-              <h5 className="text-xl font-semibold text-indigo-700 mb-3 border-b border-indigo-100 pb-2">
-                {post.title}
-              </h5>
-              <p className="text-gray-700 leading-relaxed mt-2">{post.body}</p>
-            </article>
-          )
-        })}
+    <div className='flex flex-wrap gap-4 justify-center items-start'>
+       { isLoading && <h3>Loading...</h3>}
+       {error && <h4>{error}</h4>}
+       {posts && posts.map((post , index) =>{
+        return <article key={index} className="flex w-[500px] flex-col bg-white rounded-2xl shadow-md p-5 hover:shadow-lg transition-shadow duration-300">
+  <h5 className="text-xl font-semibold text-indigo-700 mb-3 border-b border-indigo-100 pb-2">
+    {post.title}
+  </h5>
+  <p className="text-gray-700 leading-relaxed mt-2">
+    {post.body}
+  </p>
+  <button onClick={() =>handleDelete(post.id)} className='bg-red-200 rounded-2xl p-2 cursor-pointer'>Delete</button>
+</article>
+       })}
     </div>
   )
 }
--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -2,12 +2,18 @@
 import axios from 'axios'
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-
+const base_url = 'https://jsonplaceholder.typicode.com/posts'
 export const fetchPosts = createAsyncThunk('posts/fetchPosts' , async() =>{
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    const response = await axios.get(base_url)
     return response.data
+    
 })
-
+export const deletePost = createAsyncThunk('posts/deletePost' , async (id) =>{
+    await axios(`${base_url}/${id}`,{
+        method : 'Delete'
+    })
+    return id
+})
 const postsSlice = createSlice({
     name : 'posts',
     initialState :{
@@ -18,16 +24,19 @@ const postsSlice = createSlice({
     extraReducers :(builder) =>{
         builder
             .addCase(fetchPosts.pending ,(state) =>{
-            state.loading = true
+            state.isLoading = true
             state.error = null
             })
             .addCase(fetchPosts.fulfilled , (state , action) =>{
-                state.loading = false
+                state.isLoading = false
                 state.posts = action.payload
             })
             .addCase(fetchPosts.rejected , (state , action) =>{
-                state.loading = false
+                state.isLoading = false
                 state.error = action.error.message
+            })
+            .addCase(deletePost.fulfilled ,(state , action) =>{
+                state.posts = state.posts.filter(p => p.id !== action.payload)
             })
     }
 
